fcec8a210aa8e90f7bf6a41e7c5e87c022056af9
Yaroslav Olesiiuk <olesiiuk.yaroslav@gmail.com>
2018-07-16 23:29:02 +0300

    RegEx has been written

--- src/main/java/ua/kpi/tef2/controller/InputValidator.java
+++ src/main/java/ua/kpi/tef2/controller/InputValidator.java
@@ -1,8 +1,32 @@
 package ua.kpi.tef2.controller;
 
 public class InputValidator {
+    private static final String NAME_AND_LAST_NAME_REG_EX = "^[A-Z]([a-z][^(-){2,}\\s.]?)+$";
+    private static final String COMMENT_REG_EX = "^[\\w\\s.,!?:…]{0,500}$";
+    private static final String PHONE_REG_EX = "^(\\+\\d\\d-)?(\\d\\d\\d)-(\\d\\d\\d)(-\\d\\d){2}$";
+    private static final String EMAIL_REG_EX = "^[\\w.]{3,15}@[a-zA-Z.]{4,15}$";
+
+    private static final String NAME = "Name";
+    private static final String EMAIL = "Email";
+    private static final String PHONE = "Phone";
+    private static final String COMMENT = "Comment";
 
     public static boolean validate(String inputType, String input) {
-        return true;
+        String regEx = "";
+        switch (inputType) {
+            case NAME:
+                regEx = NAME_AND_LAST_NAME_REG_EX;
+                break;
+            case COMMENT:
+                regEx = COMMENT_REG_EX;
+                break;
+            case PHONE:
+                regEx = PHONE_REG_EX;
+                break;
+            case EMAIL:
+                regEx = EMAIL_REG_EX;
+                break;
+        }
+        return input.matches(regEx);
     }
 }
--- src/test/java/ua/kpi/tef2/controller/InputValidatorTest.java
+++ src/test/java/ua/kpi/tef2/controller/InputValidatorTest.java
@@ -7,13 +7,46 @@ import static org.junit.Assert.*;
 public class InputValidatorTest {
 
     @Test
-    public void testValidation() {
+    public void testValidationNameFalse() {
         String inputType = "Name";
-        String input = "Yaroslav";
 
-        boolean isValid = InputValidator.validate(inputType, input);
-
-        assertTrue(isValid);
+        assertFalse(InputValidator.validate(inputType, "yaroslav"));
+        assertFalse(InputValidator.validate(inputType, "Yaros lav"));
+        assertFalse(InputValidator.validate(inputType, "Yaros--lav"));
+        assertFalse(InputValidator.validate(inputType, "Yaros.lav"));
     }
+
+    @Test
+    public void testNewValidationNameTrue() {
+        assertTrue(InputValidator.validate("Name", "Yaroslav"));
+        assertTrue(InputValidator.validate("Name", "Yaros-lav"));
+    }
+
+    @Test
+    public void testCommentValidation() {
+        assertTrue(InputValidator.validate("Comment", "some comment of few words"));
+        assertTrue(InputValidator.validate("Comment", "some comment of few words, and some ? " +
+                "punctuation. that ... I need to check!"));
+    }
+
+    @Test
+    public void testPhoneNumberValidation() {
+        assertTrue(InputValidator.validate("Phone", "063-845-62-45"));
+        assertTrue(InputValidator.validate("Phone", "+38-063-845-62-45"));
+        assertFalse(InputValidator.validate("Phone", "+38-abd-845-62-45"));
+        assertFalse(InputValidator.validate("Phone", "+38-063-845-62-450"));
+        assertFalse(InputValidator.validate("Phone", "+38-063-845-62--5"));
+        assertFalse(InputValidator.validate("Phone", "+38-063 845-62-45"));
+    }
+
+    @Test
+    public void testEmailValidation() {
+        assertTrue(InputValidator.validate("Email", "test@email.com"));
+        assertTrue(InputValidator.validate("Email", "te.s_t@email.com"));
+        assertFalse(InputValidator.validate("Email", "t/e.s_t@email.com"));
+        assertFalse(InputValidator.validate("Email", "te.s_t@ema il.com"));
+        assertFalse(InputValidator.validate("Email", "tes t@ema il.com"));
+    }
+    
 
 }
